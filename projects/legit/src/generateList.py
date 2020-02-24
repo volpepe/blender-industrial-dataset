866499e14da2713abefdb09671fa5c3a7e872ad2
volpepe <volpepe.prog@gmail.com>
2020-02-24 17:24:29 +0100

    Fixed save path for filelists

--- projects/legit/src/generateList.py
+++ projects/legit/src/generateList.py
@@ -8,7 +8,7 @@ import csv
 parser = argparse.ArgumentParser(
     description = 'Create train and test lists for custom dataset',
 )
-parser.add_argument('--folder', '-f', type=str, default='../../data/custom/rawframes/')
+parser.add_argument('--folder', '-f', type=str, default='../../data/custom/rawframes')
 args = parser.parse_args(sys.argv[1:])
 
 #get 70% of videos for train and 30% for val
@@ -26,18 +26,20 @@ with open('classInd.txt', 'r') as classInd:
     for cnt, line in enumerate(classInd):
         classes[line.split()[1]] = line.split()[0]
 
-with open(os.path.sep.join(args.folder.split(os.path.sep)[:-1], 'custom_train_rawframes.txt'), 'w') as trainList:
+with open(os.path.sep.join(args.folder.split(os.path.sep)[:-1]) + os.path.sep + 'custom_train_rawframes.txt', 'w') as trainList:
     for vid in train:
         trainList.write("{} {} {}\n".format(os.path.join(vid.split(os.path.sep)[-2], 
             vid.split(os.path.sep)[-1]),
             len(os.listdir(vid)),
             classes[vid.split(os.path.sep)[-2]]))
 
-with open(os.path.sep.join(args.folder.split(os.path.sep)[:-1], 'custom_val_rawframes.txt'), 'w') as valList:
+with open(os.path.sep.join(args.folder.split(os.path.sep)[:-1]) + os.path.sep + 'custom_val_rawframes.txt', 'w') as valList:
     for vid in val:
         valList.write("{} {} {}\n".format(os.path.join(vid.split(os.path.sep)[-2], 
             vid.split(os.path.sep)[-1]),
             len(os.listdir(vid)),
             classes[vid.split(os.path.sep)[-2]]))
 
-print("Generated filelists!")
+print("Generated filelists at {} and {}!".format(
+        os.path.sep.join(args.folder.split(os.path.sep)[:-1]) + os.path.sep + 'custom_val_rawframes.txt',
+        os.path.sep.join(args.folder.split(os.path.sep)[:-1]) + os.path.sep + 'custom_train_rawframes.txt'))
