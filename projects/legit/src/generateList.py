ae0b04d53c66eef7f1113b56ebe67e98533f6c50
volpepe <volpepe.prog@gmail.com>
2020-02-25 12:30:32 +0100

    re-fixed previous mistake

--- projects/legit/src/generateList.py
+++ projects/legit/src/generateList.py
@@ -20,7 +20,7 @@ current_dir = os.getcwd()
 os.chdir(args.folder)
 
 #get 70% of videos for train and 30% for val
-videos = glob.glob('*/*/')
+videos = glob.glob('*/*')
 print("Found: " + str(len(videos)) + " videos")
 k = int(len(videos) * 70 / 100)
 print("{} videos are for training and {} for validation".format(k, len(videos) - k))
@@ -28,15 +28,21 @@ print("{} videos are for training and {} for validation".format(k, len(videos) -
 train = random.sample(videos, k)
 val = [vid for vid in videos if vid not in train]
 
+print("Train set:\n")
+
 with open(os.path.sep.join(os.getcwd().split(os.path.sep)[:-1]) + os.path.sep + 'custom_train_rawframes.txt', 'w') as trainList:
     for vid in train:
+        print(vid)
         trainList.write("{} {} {}\n".format(os.path.join(vid.split(os.path.sep)[-2], 
             vid.split(os.path.sep)[-1]),
             len(os.listdir(vid)) - 1,
             classes[vid.split(os.path.sep)[-2]]))
 
+print("Val set:\n")
+
 with open(os.path.sep.join(os.getcwd().split(os.path.sep)[:-1]) + os.path.sep + 'custom_val_rawframes.txt', 'w') as valList:
     for vid in val:
+        print(vid)
         valList.write("{} {} {}\n".format(os.path.join(vid.split(os.path.sep)[-2], 
             vid.split(os.path.sep)[-1]),
             len(os.listdir(vid)) - 1,
